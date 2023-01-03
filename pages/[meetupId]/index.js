8c43860340a82a614ab67de37771286e73bf66b8
cuong1806 <cuong.dz.1806@gmail.com>
2023-01-03 09:13:00 +0700

    initial the first nextjs mini project

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/6/69/Bloem_van_een_kerstroos_%28Helleborus_niger%29_16-12-2021._%28d.j.b%29_02.jpg"
+      title="Test Meetup"
+      address="Somewhere"
+      description="This is the test meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
